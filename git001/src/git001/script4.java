782ecb71558e9995b8677c2f5c37709d3c76fc63
DELL <DELL@DESKTOP-9LVK55H.mshome.net>
2024-08-15 14:50:20 +0530

    script4

---
+++ git001/src/git001/script4.java
@@ -0,0 +1,10 @@
+package git001;
+
+public class script4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hi hello how are you?");
+	}
+
+}
